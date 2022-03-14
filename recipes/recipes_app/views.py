40d5cc34da7f5659bf071110107c519d1c228946
Adrien Milis <amilis@student.s19.be>
2022-03-14 15:37:47 +0100

    started working on forms

--- recipes/recipes_app/views.py
+++ recipes/recipes_app/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-# from django.http import
+from django.http import HttpResponse
 
-# Create your views here.
+from .forms import IngredientForm
 
 def index(request):
 
-    # template_name = 'index.html'
-    return render(request, 'recipes_app/index.html')
+
+    if request.method == 'POST':
+        form = IngredientForm(request.POST)
+        
+        if (form.is_valid()):
+            print(form.cleaned_data)
+            return HttpResponse('thanks bruv')
+
+    else:
+        form = IngredientForm()
+
+    return render(request, 'recipes_app/index.html', {'form': form})
